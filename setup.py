86500f07701a8645c385e1bf14bfcb685c743024
Tayler Porter <taylerporter@gmail.com>
2021-11-03 15:09:09 +0900

    Updating notes, added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+  name = 'pyVoIP',
+  version = '1.5.0',
+  description = 'PyVoIP is a pure python VoIP/SIP/RTP library.',
+  author = 'Tayler Porter',
+  author_email = 'taylerporter@gmail.com',
+  url = 'https://github.com/tayler6000/pyVoIP',
+  packages = find_packages()
+)
