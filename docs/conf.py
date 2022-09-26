170fe843a3c5866e00d85eb5cc42314ff5f13c77
Tayler Porter <taylerporter@gmail.com>
2022-09-25 21:01:02 -0500

    [CHANGE] Changed version to v1.6.1

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2022, Tayler Porter'
 author = 'Tayler J Porter'
 
 # The full version, including alpha/beta/rc tags
-release = '1.6.0'
+release = '1.6.1'
 
 master_doc = 'index'
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding='utf-8') as f:
 
 setup(
     name='pyVoIP',
-    version='1.6.0',
+    version='1.6.1',
     description='PyVoIP is a pure python VoIP/SIP/RTP library.',
     long_description=long_description,
     long_description_content_type="text/markdown",
