6e7fde62355c232d97df323ee9e5e1bf28a3768b
TJ Porter <taylerporter@gmail.com>
2023-05-06 03:31:56 -0500

    [ADD] Added make call tests.

--- tests/test_functionality.py
+++ tests/test_functionality.py
@@ -1,10 +1,25 @@
-from pyVoIP.VoIP import VoIPPhone, PhoneStatus
+from pyVoIP.VoIP import VoIPPhone, PhoneStatus, CallState
 import pytest
 import sys
 import time
 
 TEST_CONDITION = ("--check-functionality" not in sys.argv and "--check-func" not in sys.argv)
 REASON = "Not checking functionality"
+
+@pytest.fixture
+def phone():
+    phone = VoIPPhone("127.0.0.1", 5060, "pass", "Testing123!", myIP="127.0.0.1", sipPort=5059)
+    phone.start()
+    yield phone
+    phone.stop()
+
+
+@pytest.fixture
+def nopass_phone():
+    phone = VoIPPhone("127.0.0.1", 5060, "nopass", "", myIP="127.0.0.1", sipPort=5059)
+    phone.start()
+    yield phone
+    phone.stop()
 
 @pytest.mark.skipif(TEST_CONDITION, reason=REASON)
 def test_nopass():
@@ -31,3 +46,31 @@ def test_pass():
     while phone.get_status() == PhoneStatus.DEREGISTERING:
         time.sleep(.1)
     assert phone.get_status() == PhoneStatus.INACTIVE
+
+@pytest.mark.skipif(TEST_CONDITION, reason=REASON)
+def test_make_call(phone):
+    call = phone.call("answerme")
+    while call.state == CallState.DIALING:
+        time.sleep(.1)
+    assert call.state == CallState.ANSWERED
+    call.hangup()
+    assert call.state == CallState.ENDED
+
+@pytest.mark.skipif(TEST_CONDITION, reason=REASON)
+def test_make_nopass_call(nopass_phone):
+    call = nopass_phone.call("answerme")
+    while call.state == CallState.DIALING:
+        time.sleep(.1)
+    assert call.state == CallState.ANSWERED
+    call.hangup()
+    assert call.state == CallState.ENDED
+
+@pytest.mark.skip
+@pytest.mark.skipif(TEST_CONDITION, reason=REASON)
+def test_remote_hangup(phone):
+    call = phone.call("answerme")
+    while call.state == CallState.DIALING:
+        time.sleep(.1)
+    assert call.state == CallState.ANSWERED
+    time.sleep(5)
+    assert call.state == CallState.ENDED
