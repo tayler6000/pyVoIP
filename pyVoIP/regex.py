4e20d0bac1fc5216071af6567568c1ef46614a38
Rin Arakaki <rnarkkx@gmail.com>
2025-02-01 15:50:52 +0900

    Allow hyphens in host

--- pyVoIP/regex.py
+++ pyVoIP/regex.py
@@ -14,10 +14,10 @@ Match = re.Match
 AUTH_MATCH = re.compile(r'(\w+)=("[^"]+"|[^ \t,]+)')
 VIA_SPLIT = re.compile(r" |;")
 TO_FROM_MATCH = re.compile(
-    r'(?P<display_name>"?[\w ]*"? )?<?(?P<uri_type>sips?):(?P<user>[\w+]+)(?P<password>:\w+)?@(?P<host>[\w.]+)(?P<port>:[0-9]+)?>?'
+    r'(?P<display_name>"?[\w ]*"? )?<?(?P<uri_type>sips?):(?P<user>[\w+]+)(?P<password>:\w+)?@(?P<host>[\w.\-]+)(?P<port>:[0-9]+)?>?'
 )
 TO_FROM_DIRECT_MATCH = re.compile(
-    r'(?P<display_name>"?[\w ]*"? )?<?(?P<uri_type>sips?):(?P<host>[\w.]+)(?P<port>:[0-9]+)?>?'
+    r'(?P<display_name>"?[\w ]*"? )?<?(?P<uri_type>sips?):(?P<host>[\w.\-]+)(?P<port>:[0-9]+)?>?'
 )
 SDP_A_SPLIT = re.compile(" |/")
 SIP_VERSION_MATCH = re.compile(r"(?:SIP|sip)/[0-9.]+")
